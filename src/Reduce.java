56acfc76270e0267489ed464bd4ea3097f1c8849
omina26 <omina.nematova@mail.utoronto.ca>
2023-09-11 10:45:29 -0400

    created main method that prints how many steps it takes to reach 0 if you start at 100

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int steps = 0;
+        while (i > 0) {
+
+            if (i % 2 == 0) {
+                i = i / 2;
+                steps++;
+            } else {
+                i = i - 1;
+                steps++;
+            }
+        }
+        System.out.println(steps);
+    }
+}
